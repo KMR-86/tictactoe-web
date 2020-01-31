032c78cc7d7095649f6da646690dbde018e15eea
mushi1002 <mahmud.mushfique@gmail.com>
2020-01-31 20:49:07 +0600

    the AI is working, now you can only play against the AI and you will always have the first move

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 var gameBoard;
 cells=document.querySelectorAll('.cell');
 var whichPlayerFlag=0;
-var gameState = new Array(9);
+var gameBoard = new Array(9);
 var isGameFinished=false;
+var huPlayer;
+var aiPlayer;
 startGame();
 
 function startGame(){
@@ -11,63 +13,183 @@ function startGame(){
         cells[i].innerText="";
         cells[i].style.color="#000000";
         //console.log(cells[i].id);
-        gameState[i]="";
+        gameBoard[i]=i;
 
+    }
+    if(whichPlayerFlag%2==0){
+        huPlayer="X";
+        aiPlayer="O";
+    }
+    else{
+        huPlayer="O";
+        aiPlayer="X";
     }
     
 }
 function clicked(cell){
     //console.log(cell.target.id);
     id=cell.target.id;
-    if(document.getElementById(id).innerText=="" && !checkIfGameFinished()){
+    
+    console.log(isGameFinished);
+    if(document.getElementById(id).innerText=="" && !checkIfGameFinished(gameBoard)[0]){
         if(whichPlayerFlag%2==0){
             document.getElementById(id).innerText="X";
-            gameState[id]="X";
+            gameBoard[id]="X";
         }
         else{
             document.getElementById(id).innerText="O";
-            gameState[id]="O";
+            gameBoard[id]="O";
         }
         whichPlayerFlag++;
-        console.log(gameState);
+        console.log(gameBoard);
     }
-    if(checkIfGameFinished()){
+    if(checkIfGameFinished(gameBoard)[0]){
+        console.log("gameOver");
+    }
+    else if(emptySquares().length!=0){
+        x=minimax(gameBoard,aiPlayer);
+        makemove(x.index,aiPlayer);
+    }
+    else{
+        console.log("Game Drawn");
+    }
+    
+}
+function makemove(cellno,move){
+    console.log("the index is ",cellno);
+    if(document.getElementById(cellno).innerText=="" && !checkIfGameFinished(gameBoard)[0]){
+
+        document.getElementById(cellno).innerText=move;
+        gameBoard[cellno]=move;
+        whichPlayerFlag++;
+        console.log(gameBoard);
+    }
+    if(checkIfGameFinished(gameBoard)[0]){
         console.log("gameOver");
     }
 }
-function checkIfGameFinished(){
+function checkIfGameFinished(newgameBoard){
     for(var i=0;i<3;i++){
-        if(gameState[i*3]==gameState[i*3+1] && gameState[i*3+1]==gameState[i*3+2] && gameState[i*3]!=""){
-            isGameFinished=true;
+        if(newgameBoard[i*3]==newgameBoard[i*3+1] && newgameBoard[i*3+1]==newgameBoard[i*3+2] && newgameBoard[i*3]!=""){
+            //console.log("here1 ",i);
+            
             cells[i*3].style.color="#ff0000";
             cells[i*3+1].style.color="#ff0000";
             cells[i*3+2].style.color="#ff0000";
-            return true;
+            return [true,newgameBoard[i*3]];
         }
-        if(gameState[i]==gameState[i+3] && gameState[i+3]==gameState[i+6] && gameState[i]!=""){
-            isGameFinished=true;
+        if(newgameBoard[i]==newgameBoard[i+3] && newgameBoard[i+3]==newgameBoard[i+6] && newgameBoard[i]!=""){
+            //console.log("here2 ",i);
             cells[i].style.color="#ff0000";
             cells[i+3].style.color="#ff0000";
             cells[i+6].style.color="#ff0000";
-            return true;
+            return [true,newgameBoard[i]];
         }
 
         
     }
-    if(gameState[0]==gameState[4] && gameState[4]==gameState[8] && gameState[0]!=""){
+    if(newgameBoard[0]==newgameBoard[4] && newgameBoard[4]==newgameBoard[8] && newgameBoard[0]!=""){
         isGameFinished=true;
+        //console.log("here3 ",i);
         cells[0].style.color="#ff0000";
         cells[4].style.color="#ff0000";
         cells[8].style.color="#ff0000";
-        return true;
+        return [true,newgameBoard[0]];
     }
-    if(gameState[2]==gameState[4] && gameState[4]==gameState[6] && gameState[2]!=""){
+    if(newgameBoard[2]==newgameBoard[4] && newgameBoard[4]==newgameBoard[6] && newgameBoard[2]!=""){
         isGameFinished=true;
+        //console.log("here4 ",i);
         cells[2].style.color="#ff0000";
         cells[4].style.color="#ff0000";
         cells[6].style.color="#ff0000";
-        return true;
+        return [true,newgameBoard[2]];
     }
+    return [false,-1];
 
 }
+function justcheckIfGameFinished(newgameBoard){
+    for(var i=0;i<3;i++){
+        if(newgameBoard[i*3]==newgameBoard[i*3+1] && newgameBoard[i*3+1]==newgameBoard[i*3+2] && newgameBoard[i*3]!=""){
 
+            return [true,newgameBoard[i*3]];
+        }
+        if(newgameBoard[i]==newgameBoard[i+3] && newgameBoard[i+3]==newgameBoard[i+6] && newgameBoard[i]!=""){
+
+            return [true,newgameBoard[i]];
+        }
+
+        
+    }
+    if(newgameBoard[0]==newgameBoard[4] && newgameBoard[4]==newgameBoard[8] && newgameBoard[0]!=""){
+        return [true,newgameBoard[0]];
+    }
+    if(newgameBoard[2]==newgameBoard[4] && newgameBoard[4]==newgameBoard[6] && newgameBoard[2]!=""){
+        return [true,newgameBoard[2]];
+    }
+    return [false,-1];
+
+}
+function emptySquares(){
+    emptyList=[];
+    for(var i=0;i<gameBoard.length;i++){
+        if(gameBoard[i]!="X" && gameBoard[i]!="O")emptyList.push(i);
+    }
+    return emptyList;
+}
+
+function minimax(newBoard, player) {
+	var availSpots = emptySquares();
+
+	if (justcheckIfGameFinished(newBoard)[0]==true && justcheckIfGameFinished(newBoard)[1]==huPlayer) {
+		return {score: -10};
+	} else if (justcheckIfGameFinished(newBoard)[0]==true && justcheckIfGameFinished(newBoard)[1]==aiPlayer) {
+		return {score: 10};
+	} else if (availSpots.length === 0) {
+        
+		return {score: 0};
+	}
+	var moves = [];
+	for (var i = 0; i < availSpots.length; i++) {
+		var move = {};
+		move.index = availSpots[i];
+		newBoard[availSpots[i]] = player;
+
+		if (player == aiPlayer) {
+			var result = minimax(newBoard, huPlayer);
+            move.score = result.score;
+
+            
+		} else {
+			var result = minimax(newBoard, aiPlayer);
+            move.score = result.score;
+
+		}
+
+		newBoard[availSpots[i]] = move.index;
+
+        moves.push(move);
+        
+	}
+
+	var bestMove=888;
+	if(player === aiPlayer) {
+		var bestScore = -10000;
+		for(var i = 0; i < moves.length; i++) {
+            
+			if (moves[i].score > bestScore) {
+				bestScore = moves[i].score;
+				bestMove = i;
+			}
+		}
+	} else {
+		var bestScore = 10000;
+		for(var i = 0; i < moves.length; i++) {
+			if (moves[i].score < bestScore) {
+				bestScore = moves[i].score;
+				bestMove = i;
+			}
+		}
+	}
+    //console.log("the best move score is ",bestMove,"   ",moves.length,"   ",moves[bestMove].index);
+	return moves[bestMove];
+}
